7d1a4abbdf24114e115593c7b87751e1fd50b758
Henrique Linhares <linhares.h@gmail.com>
2019-01-30 18:10:57 -0200

    Evolving in asking 'the wrong data' part of the provenance enhancement

--- pyalgdb/provenance_enhancement.py
+++ pyalgdb/provenance_enhancement.py
@@ -6,6 +6,7 @@ from pyalgdb.validity import Validity
 from pyalgdb.execution_tree import ExecutionTree
 from pyalgdb.visualization import Visualization
 from pyalgdb.provenance_tools import ProvenanceTools
+from pyalgdb.evaluation import Evaluation
 
 class ProvenanceEnhancement(NavigationStrategy):
 
@@ -14,29 +15,27 @@ class ProvenanceEnhancement(NavigationStrategy):
         self.prov_tools = ProvenanceTools(cursor)
         self.cursor = cursor
         self.dependencies =[]
+        self.filtered_dependencies = []
 
     def ask_wrong_data(self):
         ans = input("Which output data is not correct? ")
-        query = ("select CC.name, CC.id, CC.first_char_line "
+        query = ("select EVAL.id, CC.id "
                  "from evaluation EVAL "
                  "join code_component CC on EVAL.code_component_id = CC.id " 
                  "where EVAL.repr = ? ")
-        code_components = []
-        print("Select the invalid evaluation")
-        i = 1
+        evals = []
         for tupl in self.cursor.execute(query, [ans]):
-            print(str(i)+". " + str(tupl[0])+ " = " + ans + " at line " + str(tupl[2]))
-            i += 1
-            code_components.append(tupl)
-        invalid_eval = int(input()) - 1
-        invalid_cc = code_components[invalid_eval]
-        return CodeComponent(invalid_cc[1], invalid_cc[0], "STARTER")
+            evals.append(Evaluation(tupl[0], tupl[1]))
+        return evals[-1]
 
-    def enhance(self):
+    def search_influencers_of(self, ev_id):
         for d in self.dependencies:
-            source_nodes = self.exec_tree.search_by_ev_id(d.source.ev_id)
-            target_nodes = self.exec_tree.search_by_ev_id(d.target.ev_id)
-            for sn in source_nodes:
-                for tn in target_nodes:
-                    sn.prov = True
-                    tn.prov = True
+            if d.target.ev_id == ev_id:
+                self.filtered_dependencies.append(d)
+                self.search_influencers_of(d.source.ev_id)
+
+    def enhance(self, evaluation):
+        self.search_influencers_of(evaluation.ev_id)
+        
+
+    
