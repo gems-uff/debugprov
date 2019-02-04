5bc9dcaf7b94a0477bbfb8ae662efc1c8699db9e
Henrique Linhares <linhares.h@gmail.com>
2019-02-04 18:52:10 -0200

    Updating tests

--- pyalgdb/tests/test_dependency_rel.py
+++ pyalgdb/tests/test_dependency_rel.py
@@ -2,16 +2,16 @@ import unittest
 import sys
 
 from pyalgdb.dependency_rel import DependencyRel
-from pyalgdb.code_component import CodeComponent
+from pyalgdb.evaluation import Evaluation
 
 class DependencyRelTest(unittest.TestCase):
 
     def test_dependency_rel(self):    
-        one_cc = CodeComponent(100, "100#code_component_name", "100#code_component_typeof")
-        other_cc = CodeComponent(200, "200#code_component_name", "200#code_component_typeof")
-        dep = DependencyRel(one_cc, other_cc)
-        self.assertEqual(dep.source, one_cc)
-        self.assertEqual(dep.target, other_cc)
+        influencer = Evaluation(100, 101, 'call', '#name-influencer')
+        dependent = Evaluation(200, 201, 'variable', '#name-dependent')
+        dep = DependencyRel(influencer, dependent)
+        self.assertEqual(dep.influencer, influencer)
+        self.assertEqual(dep.dependent, dependent)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ pyalgdb/tests/test_evaluation.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyalgdb.evaluation import Evaluation
+
+class EvaluationTest(unittest.TestCase):
+
+    def test_evaluation(self):
+        evaluation_id = 300
+        code_component_id = 301
+        code_component_typeof = "300#CodeComponentTypeof"
+        code_component_name = "300#CodeComponentName"
+        evl = Evaluation(evaluation_id, code_component_id, code_component_typeof,code_component_name)
+        
+        self.assertEqual(evl.ev_id, evaluation_id)
+        self.assertEqual(evl.code_component_id, code_component_id)
+        self.assertEqual(evl.code_component_type, code_component_typeof)
+        self.assertEqual(evl.code_component_name, code_component_name)
+
+        
+if __name__ == '__main__':
+    unittest.main()
