b1444110c74689ec3635399c823b02a6d727bf4f
Henrique Linhares <linhares.h@gmail.com>
2019-02-06 14:23:25 -0200

    Updating the visualization.py class, and making changes in the notebooks to adapt to these changes in visualization

--- pyalgdb/visualization.py
+++ pyalgdb/visualization.py
@@ -7,10 +7,11 @@ from pyalgdb.validity import Validity
 class Visualization:
 
     BUGGY_NODE_COLOR = 'red'
-    PROVENANCE_COLOR = 'dodgerblue'
+    PROVENANCE_EDGE_COLOR = 'dodgerblue'
     INVALID_COLOR = 'darkorange1'
     VALID_COLOR = 'darkolivegreen1'
-    NODE_IN_EVALUATION = 'gold2'
+    NODE_IN_EVALUATION_COLOR = 'gold2'
+    PROV_PRUNED_NODE_COLOR = 'grey55'
 
     def __init__(self, exec_tree: ExecutionTree):
         self.exec_tree = exec_tree
@@ -25,13 +26,13 @@ class Visualization:
         self.navigate(root_node)
         eval_node = self.exec_tree.node_under_evaluation
         if eval_node is not None:
-            self.graph.node(str(eval_node.ev_id), str(eval_node.get_name()), fillcolor=self.NODE_IN_EVALUATION, style='filled')
+            self.graph.node(str(eval_node.ev_id), str(eval_node.get_name()), fillcolor=self.NODE_IN_EVALUATION_COLOR, style='filled')
         buggy_node = self.exec_tree.buggy_node
         if buggy_node is not None:
             self.graph.node(str(buggy_node.ev_id), str(buggy_node.get_name()), fillcolor=self.BUGGY_NODE_COLOR, style='filled')
         if self.exec_tree.dependencies is not None:
             for d in self.exec_tree.dependencies: # this loop draws the provenance links between nodes
-                self.graph.edge(str(d.dependent.ev_id), str(d.influencer.ev_id), None, color=self.PROVENANCE_COLOR, dir='forward')
+                self.graph.edge(str(d.dependent.ev_id), str(d.influencer.ev_id), None, color=self.PROVENANCE_EDGE_COLOR, dir='forward')
 
     def view_exec_tree(self, graph_name = 'exec_tree'):
         self.generate_exec_tree(graph_name)
@@ -47,7 +48,9 @@ class Visualization:
                 self.graph.node(str(n.ev_id), str(n.get_name()), fillcolor=self.VALID_COLOR, style='filled')
             elif n.validity == Validity.UNKNOWN:  
                 self.graph.node(str(n.ev_id), str(n.get_name()))
-
+            elif n.validity is Validity.NOT_IN_PROV:
+                self.graph.node(str(n.ev_id), str(n.get_name()), fillcolor=self.PROV_PRUNED_NODE_COLOR, style='filled')
+                
         if len(chds) > 0:
             g = Graph()
             for c in chds:
