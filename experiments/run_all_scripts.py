df8a3856098a528f13cfd2adf2605d41d5ac444b
Henrique Linhares <linhares.h@gmail.com>
2019-03-13 11:42:13 -0300

    Adding more scripts and about.md's to the experiments directory

--- experiments/run_all_scripts.py
+++ experiments/run_all_scripts.py
@@ -14,13 +14,17 @@ scripts = ['01-compression_analysis/psnr.py',
            '04-lu_decomposition/lu_decomposition.py',
            '05-newton_method/newton_method.py',
            '06-md5/hashmd5.py',
+           '07-basic_binary_tree/basic_binary_tree.py',
+           '08-edit_distance/edit_distance.py',
            '09-dijkstra_algorithm/dijkstra_algorithm.py',
            '10-caesar_cipher/caesar_cipher.py',
            '11-brute_force_caesar_cipher/brute_force_caesar_cipher.py',
            '12-basic_maths/basic_maths.py',
            '13-merge_sort/merge_sort.py',
+           '14-rsa_cipher/rsa_cipher.py',
            '15-decision_tree/decision_tree.py',
            '16-math_parser/math_parser.py',
+           '17-merge_intervals/merge_intervals.py'
            '19-binary_search/binary_search.py',
            '21-longest_common_subsequence/lcs.py',
            '24-bubblesort/bubblesort.py',
@@ -28,9 +32,13 @@ scripts = ['01-compression_analysis/psnr.py',
            '27-generate_parenthesis/generate_parenthesis.py']
 
 def run_scripts(scripts,save_log=False):
-    for script in scripts:
-        print("$ python "+script)
+    for script_path in scripts:
+        c += 1
+        print("$ python "+script_path)
+        directory = script_path.split('/')[0]
+        script = script_path.split('/')[1]
         try:
+            os.chdir(directory)
             proc = subprocess.Popen(['python',script], cwd=os.getcwd(), env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = proc.communicate()
             print(stdout.decode('utf-8'))
@@ -38,9 +46,11 @@ def run_scripts(scripts,save_log=False):
                 logfile = open(script+'.log','w') 
                 logfile.write(stdout.decode('utf-8'))
                 logfile.close()
+            os.chdir('..')
         except:
             print("#### "+script)
             print('#### something went very very wrong')
-
+            os.chdir('..')
+    
 os.chdir(SCRIPTS_DIRECTORY)
 run_scripts(scripts,True)
