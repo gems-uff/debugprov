168b0eaae821cc4a5b49d598e3f480998f475e92
Henrique Linhares <linhares.h@gmail.com>
2019-03-13 12:25:48 -0300

    Almost finishing to add scripts to experiments/

--- experiments/run_all_scripts.py
+++ experiments/run_all_scripts.py
@@ -24,24 +24,34 @@ scripts = ['01-compression_analysis/psnr.py',
            '14-rsa_cipher/rsa_cipher.py',
            '15-decision_tree/decision_tree.py',
            '16-math_parser/math_parser.py',
-           '17-merge_intervals/merge_intervals.py'
+           '17-merge_intervals/merge_intervals.py',
+           '18-graph_find_path/find_path.py',
            '19-binary_search/binary_search.py',
+           '20-permute/permute.py',
            '21-longest_common_subsequence/lcs.py',
+           '22-catalan/catalan.py',
+           '23-longest_increasing_subsequence/lis.py',
            '24-bubblesort/bubblesort.py',
            '25-quicksort/quicksort.py',
-           '27-generate_parenthesis/generate_parenthesis.py']
+           '26-heapsort/heapsort.py',
+           '27-generate_parenthesis/generate_parenthesis.py',
+           '29-string_permutation/stringpermutation.py',
+           '30-linear_regression/demo.py']
 
 def run_scripts(scripts,save_log=False):
+    c=0
     for script_path in scripts:
         c += 1
-        print("$ python "+script_path)
+        #print(c)
+        print(script_path)
+        #print("$ python "+script_path)
         directory = script_path.split('/')[0]
         script = script_path.split('/')[1]
         try:
             os.chdir(directory)
             proc = subprocess.Popen(['python',script], cwd=os.getcwd(), env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = proc.communicate()
-            print(stdout.decode('utf-8'))
+            #print(stdout.decode('utf-8'))
             if save_log:
                 logfile = open(script+'.log','w') 
                 logfile.write(stdout.decode('utf-8'))
@@ -51,6 +61,7 @@ def run_scripts(scripts,save_log=False):
             print("#### "+script)
             print('#### something went very very wrong')
             os.chdir('..')
+    #print(c)
     
 os.chdir(SCRIPTS_DIRECTORY)
 run_scripts(scripts,True)
