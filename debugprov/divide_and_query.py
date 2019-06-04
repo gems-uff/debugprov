0489e4c050266a02f11557c95e36168b5629aca2
Henrique Linhares <linhares.h@gmail.com>
2019-06-04 18:58:24 -0300

    Changes in divide and query

--- debugprov/divide_and_query.py
+++ debugprov/divide_and_query.py
@@ -9,30 +9,46 @@ class DivideAndQuery(NavigationStrategy):
         self.recursive_navigate(self.exec_tree.root_node)
         return self.exec_tree
 
-    def find_best_guess(self, node:Node, w_2:float):
-        if node.weight <= w_2 and node.validity is Validity.UNKNOWN:
-            if (self.best_guess is None):
-                self.best_guess = node
-            elif node.weight > self.best_guess.weight:
-                self.best_guess = node
-        for c in node.childrens:
-            if c.validity is not Validity.VALID:
-                self.find_best_guess(c, w_2)
+
+    def find_best_guess(self,node,w_2):
+        return max(
+            (n for n in self.exec_tree.get_all_nodes()
+            if n.validity is Validity.UNKNOWN
+            if n.weight <= w_2),
+            key=lambda n: n.weight)
+        #nodes = self.exec_tree.get_all_nodes()
+        #best_guess = None
+        #for n in nodes:
+        #    if n.weight > best_guess and n.weight <= w_2 and n.validity == Validity.UNKOWN:
+        #        best_guess = n
+        #return n
+
+
+   # def find_best_guess(self, node:Node, w_2:float):
+   #     if node.weight <= w_2 and node.validity is Validity.UNKNOWN:
+   #         if (self.best_guess is None):
+   #             self.best_guess = node
+   #         elif node.weight > self.best_guess.weight:
+   #             self.best_guess = node
+   #     for c in node.childrens:
+   #         if c.validity is not Validity.VALID:
+   #             self.find_best_guess(c, w_2)
 
     def weight(self, node: Node):
         summ = 0
         for c in node.childrens:
-            if c.validity is Validity.UNKNOWN or c.validity is Validity.INVALID:
+            if c.validity is Validity.UNKNOWN:
                 summ += 1 + self.weight(c)
-            elif c.validity is Validity.NOT_IN_PROV:
+            else:
                 summ += self.weight(c)
         node.weight = summ
         return summ
 
     def recursive_navigate(self, node: Node):
-        self.weight(node)
+        self.weight(self.exec_tree.root_node)
         self.best_guess = None
-        self.find_best_guess(node, (node.weight/2))
+        self.best_guess = self.find_best_guess(self.exec_tree.root_node, (self.exec_tree.root_node.weight/2))
+        print("Best guess: {}".format(self.best_guess))
         if self.best_guess is not None:
             self.evaluate(self.best_guess)
             if self.best_guess.validity is Validity.VALID:
@@ -44,8 +60,10 @@ class DivideAndQuery(NavigationStrategy):
             elif self.best_guess.validity is Validity.INVALID:
                 # Invalidar o "resto da árvore"
                 if self.best_guess.has_childrens_with_validity(Validity.UNKNOWN):
+                    print("IF")
                     self.recursive_navigate(node)
                 else:
+                    print("ELSE")
                     self.exec_tree.buggy_node = self.best_guess
                     self.finish_navigation()
 
