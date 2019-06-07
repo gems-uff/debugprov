15cfa6efa85ba9e63aa4f3a3ee773a138abf8142
Henrique Linhares <linhares.h@gmail.com>
2019-06-07 14:35:23 -0300

    Removing print from navigation strategy

--- debugprov/navgiation_strategy.py
+++ debugprov/navgiation_strategy.py
@@ -74,7 +74,7 @@ class NavigationStrategy:
         return node
 
     def recursive_validate(self, node, forbidden=None):
-        print("RECURSIVE VALIDATE {}".format(node.ev_id))
+        #print("RECURSIVE VALIDATE {}".format(node.ev_id))
         if node is forbidden:
             return
         # if node.validity is not Validity.NOT_IN_PROV:
